bbac1b6e5e4e31fe32e46c7ffde1d301bafc6f7d
Karlo <88337245+Rqnsom@users.noreply.github.com>
2022-10-21 12:42:54 +0200

    chore: avoid using unwrap
    - rather expect everything :^)

--- src/tests/conformance/handshake.rs
+++ src/tests/conformance/handshake.rs
@@ -23,18 +23,23 @@ async fn c001_handshake_when_node_receives_connection() {
         .await
         .expect("Unable to build a synthetic node");
 
+    let net_addr = node.net_addr().expect("Network address not found");
+
     // Connect to the node and initiate the handshake.
     synthetic_node
-        .connect(node.net_addr().unwrap())
+        .connect(net_addr)
         .await
-        .unwrap();
+        .expect("Unable to connect");
 
     // This is only set post-handshake (if enabled).
-    assert!(synthetic_node.is_connected(node.net_addr().unwrap()));
+    assert!(
+        synthetic_node.is_connected(net_addr),
+        "Synthetic node is not connected to the node"
+    );
 
     // Gracefully shut down the nodes.
     synthetic_node.shut_down().await;
-    node.stop().unwrap();
+    node.stop().expect("Unable to stop the node");
 }
 
 #[tokio::test]
@@ -50,7 +55,9 @@ async fn c002_handshake_when_node_initiates_connection() {
     // Spin up a node instance.
     let target = TempDir::new().expect("Couldn't create a temporary directory");
     let mut node = Node::builder()
-        .initial_peers([synthetic_node.listening_addr().unwrap()])
+        .initial_peers([synthetic_node
+            .listening_addr()
+            .expect("Listening address not found")])
         .build(target.path())
         .expect("Unable to build the node");
     node.start().await;
@@ -61,13 +68,19 @@ async fn c002_handshake_when_node_initiates_connection() {
 
     // Check the connection has been established (this is only set post-handshake). We can't check
     // for the addr as nodes use ephemeral addresses when initiating connections.
-    assert_ne!(node_addr, node.net_addr().unwrap());
+    assert_ne!(
+        node_addr,
+        node.net_addr().expect("Network address not found")
+    );
 
     // The node sends multiple get_block HTTP queries from different TCP sockets in parallel,
     // so on rare occasions we might have additional few short-lasting connections.
-    assert!(synthetic_node.num_connected() >= 1);
+    assert!(
+        synthetic_node.num_connected() >= 1,
+        "At least one connection is expected"
+    );
 
     // Gracefully shut down the nodes.
     synthetic_node.shut_down().await;
-    node.stop().unwrap();
+    node.stop().expect("Unable to stop the node");
 }
