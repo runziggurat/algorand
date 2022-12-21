6ce1e1e1607c4ea4f82ed46ac6648f5fe28314cc
Piotr Olszewski <asmie@asmie.pl>
2022-12-21 17:53:54 +0100

    refactor: rename functions and variables

--- src/tests/performance/prio_test.rs
+++ src/tests/performance/prio_test.rs
@@ -21,9 +21,8 @@ use crate::{
     setup::node::Node,
     tools::{
         constants::{
-            ERR_BIND_TO_SOCKET_FAILED, ERR_NET_ADDR_NOT_FOUND, ERR_NODE_BUILD_FAILED,
-            ERR_NODE_CONNECTION_FAILED, ERR_NODE_UNABLE_TO_STOP, ERR_SEND_MESSAGE_FAILED,
-            ERR_SYNTH_NODE_BUILD_FAILED, ERR_TEMPDIR_CREATION_FAILED,
+            ERR_NODE_ADDR, ERR_NODE_BUILD, ERR_NODE_CONNECT, ERR_NODE_STOP, ERR_SOCKET_BIND,
+            ERR_SYNTH_BUILD, ERR_SYNTH_UNICAST, ERR_TEMPDIR_NEW,
         },
         ips::IPS,
         metrics::{
@@ -103,12 +102,12 @@ const RESPONSE_TIMEOUT: Duration = Duration::from_secs(3);
 #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
 #[allow(non_snake_case)]
 async fn p002_t1_PRIO_MSG_latency() {
-    // ZG-PERFORMANCE-002, Block getting latency while malicious nodes send higher priority msgs
+    // ZG-PERFORMANCE-002, Getting low prio meesages while other nodes send higher priority msgs
     //
     // We test the overall performance of a node's get blocks (with certs) latency while other
-    // malicious nodes send higher priority messages.
+    // other nodes send higher priority messages.
     //
-    // Test should be inspected manually to check how other malicious nodes affect the latency of
+    // Test should be inspected manually to check how other nodes affect the latency of
     // the node under test.
     //
     // Sample results:
@@ -132,27 +131,25 @@ async fn p002_t1_PRIO_MSG_latency() {
     // *NOTE* run with `cargo test --release  tests::performance::prio -- --nocapture`
     // Before running test generate dummy devices with different ips using toos/ips.py
 
-    let h_prio_peers = vec![1, 50, 100, 200, 300, 400, 799];
+    let h_prio_peer_set = vec![1, 50, 100, 200, 300, 400, 799];
     let l_prio_peers = 1;
 
     let mut table = RequestsTable::default();
 
-    for hp_peer_iter_cnt in h_prio_peers {
-        // synth_count malicious tasks plus one normal synth_node
-        let barrier = Arc::new(Barrier::new(hp_peer_iter_cnt + 1));
+    for h_prio_peers in h_prio_peer_set {
+        let total_peers = l_prio_peers + h_prio_peers;
+        let barrier = Arc::new(Barrier::new(total_peers));
 
-        let target = TempDir::new().expect(ERR_TEMPDIR_CREATION_FAILED);
-        let mut node = Node::builder()
-            .build(target.path())
-            .expect(ERR_NODE_BUILD_FAILED);
+        let target = TempDir::new().expect(ERR_TEMPDIR_NEW);
+        let mut node = Node::builder().build(target.path()).expect(ERR_NODE_BUILD);
         node.start().await;
 
-        let node_addr = node.net_addr().expect(ERR_NET_ADDR_NOT_FOUND);
+        let node_addr = node.net_addr().expect(ERR_NODE_ADDR);
 
-        let mut synth_sockets = Vec::with_capacity(hp_peer_iter_cnt + 1);
+        let mut synth_sockets = Vec::with_capacity(total_peers);
         let mut ips = IPS.to_vec();
 
-        for _ in 0..hp_peer_iter_cnt + 1 {
+        for _ in 0..total_peers {
             // If there is address for our thread in the pool we can use it.
             // Otherwise we'll not set bound_addr and use local IP addr (127.0.0.1).
             let ip = ips.pop().unwrap_or("127.0.0.1");
@@ -164,7 +161,7 @@ async fn p002_t1_PRIO_MSG_latency() {
             socket.set_reuseaddr(true).unwrap();
             socket.set_reuseport(true).unwrap();
 
-            socket.bind(ip).expect(ERR_BIND_TO_SOCKET_FAILED);
+            socket.bind(ip).expect(ERR_SOCKET_BIND);
             synth_sockets.push(socket);
         }
 
@@ -177,7 +174,7 @@ async fn p002_t1_PRIO_MSG_latency() {
         let test_start = tokio::time::Instant::now();
 
         let arc_barrier = barrier.clone();
-        synth_handles.spawn(simulate_normal_peer(
+        synth_handles.spawn(simulate_low_priority_peer(
             node_addr,
             synth_sockets.pop().unwrap(),
             arc_barrier,
@@ -185,7 +182,7 @@ async fn p002_t1_PRIO_MSG_latency() {
 
         for socket in synth_sockets {
             let arc_barrier = barrier.clone();
-            synth_handles.spawn(simulate_malicious_peer(node_addr, socket, arc_barrier));
+            synth_handles.spawn(simulate_high_priority_peer(node_addr, socket, arc_barrier));
         }
 
         // wait for peers to complete
@@ -198,8 +195,8 @@ async fn p002_t1_PRIO_MSG_latency() {
             if latencies.entries() >= 1 {
                 // add stats to table display
                 table.add_row(RequestStats::new(
-                    l_prio_peers, // only one normal peer
-                    hp_peer_iter_cnt as u16,
+                    l_prio_peers as u16, // only one normal peer
+                    h_prio_peers as u16,
                     REQUESTS,
                     latencies,
                     time_taken_secs,
@@ -207,7 +204,7 @@ async fn p002_t1_PRIO_MSG_latency() {
             }
         }
 
-        node.stop().expect(ERR_NODE_UNABLE_TO_STOP);
+        node.stop().expect(ERR_NODE_STOP);
     }
 
     // Display results table
@@ -215,7 +212,7 @@ async fn p002_t1_PRIO_MSG_latency() {
 }
 
 const ROUND_KEY: Round = 1;
-async fn simulate_normal_peer(
+async fn simulate_low_priority_peer(
     node_addr: SocketAddr,
     socket: TcpSocket,
     start_barrier: Arc<Barrier>,
@@ -223,13 +220,13 @@ async fn simulate_normal_peer(
     let mut synth_node = SyntheticNodeBuilder::default()
         .build()
         .await
-        .expect(ERR_SYNTH_NODE_BUILD_FAILED);
+        .expect(ERR_SYNTH_BUILD);
 
     // Establish peer connection
     synth_node
         .connect_from(node_addr, socket)
         .await
-        .expect(ERR_NODE_CONNECTION_FAILED);
+        .expect(ERR_NODE_CONNECT);
 
     // Wait for all peers to connect
     start_barrier.wait().await;
@@ -248,7 +245,7 @@ async fn simulate_normal_peer(
 
         synth_node
             .unicast(node_addr, message)
-            .expect(ERR_SEND_MESSAGE_FAILED);
+            .expect(ERR_SYNTH_UNICAST);
 
         let now = Instant::now();
 
@@ -271,7 +268,7 @@ async fn simulate_normal_peer(
     synth_node.shut_down().await
 }
 
-async fn simulate_malicious_peer(
+async fn simulate_high_priority_peer(
     node_addr: SocketAddr,
     socket: TcpSocket,
     start_barrier: Arc<Barrier>,
@@ -279,13 +276,13 @@ async fn simulate_malicious_peer(
     let mut synth_node = SyntheticNodeBuilder::default()
         .build()
         .await
-        .expect(ERR_SYNTH_NODE_BUILD_FAILED);
+        .expect(ERR_SYNTH_BUILD);
 
     // Establish peer connection
     synth_node
         .connect_from(node_addr, socket)
         .await
-        .expect(ERR_NODE_CONNECTION_FAILED);
+        .expect(ERR_NODE_CONNECT);
 
     // Wait for all peers to start
     start_barrier.wait().await;
@@ -317,7 +314,7 @@ async fn simulate_malicious_peer(
 
         synth_node
             .unicast(node_addr, message)
-            .expect(ERR_SEND_MESSAGE_FAILED);
+            .expect(ERR_SYNTH_UNICAST);
 
         // Just check if there is anything to read in the incoming queue. If so, read and
         // discard it. We don't care about the response.
--- src/tools/constants.rs
+++ src/tools/constants.rs
@@ -9,26 +9,28 @@ pub const CONNECTION_TIMEOUT: Duration = Duration::from_secs(10);
 pub const EXPECT_MSG_TIMEOUT: Duration = Duration::from_secs(10);
 
 // Below common error constants and messages to be used across the project.
-/// Error message when binding to specified socket fails.
-pub const ERR_BIND_TO_SOCKET_FAILED: &str = "unable to bind to socket";
+// The main rule to create const names is: ERR_<WHO>_<FUNCTION_NAME>
 
-/// Error message when node network address is not found.
-pub const ERR_NET_ADDR_NOT_FOUND: &str = "network address not found";
+/// Error message when binding to specified socket fails.
+pub const ERR_SOCKET_BIND: &str = "unable to bind to socket";
+
+/// Error message when the node's network address is not found.
+pub const ERR_NODE_ADDR: &str = "network address not found";
 
 /// Error message for a failed connection.
-pub const ERR_NODE_CONNECTION_FAILED: &str = "unable to connect to the node";
+pub const ERR_NODE_CONNECT: &str = "unable to connect to the node";
 
 /// Error message when building a node fails.
-pub const ERR_NODE_BUILD_FAILED: &str = "unable to build the node";
+pub const ERR_NODE_BUILD: &str = "unable to build the node";
 
 /// Error message when a node fails to stop.
-pub const ERR_NODE_UNABLE_TO_STOP: &str = "unable to stop the node";
+pub const ERR_NODE_STOP: &str = "unable to stop the node";
 
 /// Error message when sending message fails.
-pub const ERR_SEND_MESSAGE_FAILED: &str = "unable to send message";
+pub const ERR_SYNTH_UNICAST: &str = "unable to send a message";
 
 /// Error message when synthetic node creation fails.
-pub const ERR_SYNTH_NODE_BUILD_FAILED: &str = "unable to build a synthetic node";
+pub const ERR_SYNTH_BUILD: &str = "unable to build a synthetic node";
 
 /// Error message when temporary directory creation fails.
-pub const ERR_TEMPDIR_CREATION_FAILED: &str = "couldn't create a temporary directory";
+pub const ERR_TEMPDIR_NEW: &str = "couldn't create a temporary directory";
